30903a0e2977a362f5ef933204c524e2a474589e
11918 <11918@DESKTOP-U3EMCR0>
2021-04-18 13:45:21 +0800

    Project set up

---
+++ src/main/java/astraycat/demo/SharewebApplication.java
@@ -0,0 +1,13 @@
+package astraycat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/astraycat/demo/SharewebApplicationTests.java
@@ -0,0 +1,13 @@
+package astraycat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
